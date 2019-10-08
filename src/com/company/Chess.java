74daa4e9d37cdcab6e791b890836705163aefcb0
anast <nastyatikhomirova8@gmail.com>
2019-10-08 14:46:10 +0300

    initial commit

--- src/com/company/Chess.java
+++ src/com/company/Chess.java
@@ -1,10 +1,12 @@
 package com.company;
+
 import java.util.Scanner;
 
 public class Chess {
 
     int length;
-    int height ;
+    int height;
+
     public Chess(int length, int height) {
         this.length = length;
         this.height = height;
@@ -17,34 +19,16 @@ public class Chess {
     public void setLength(int length) {
         this.length = length;
     }
+
     public int getHeight() {
         return height;
     }
 
     public void setHeight(int height) {
-        this.height= height;
+        this.height = height;
     }
 
- public  void Draw()
-{
-    int[][] f = new int[8][8];
 
-    for (int i = 0; i < length; i++) {
-        for (int j = 0; j < height; j++) {
-            if ((i + j) % 2 == 0) {
-                f[i][j] = 1;
-            } else {
-                f[i][j] = 0;
-            }
-        }
-    }
-    for (int k = 0; k < length; k++) {
-        for(int m=0;m<height;m++){
-            System.out.print(f[k][m]+" ");
-        }
-        System.out.println();
-    }
-    }
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
+    static int length;
+    static int height;
     public static void main(String[] args) {
-	int length;
-	int height;
+
         Scanner in = new Scanner(System.in);
-        while(true){
+      //  while(true){
             try {
 
                 System.out.println("Input length: ");
@@ -16,14 +16,32 @@ public class Main {
                 System.out.println("Input height: ");
                 height = in.nextInt();
                 Chess chess = new Chess(length, height);
-                chess.Draw();
+                Draw();
 
         } catch (Exception e) {
             System.out.println("Incorrect value. Input an integer.");
             in.next();
-        }
+      //  }
 
     }}
+    public static void Draw() {
+        int[][] f = new int[8][8];
 
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < height; j++) {
+                if ((i + j) % 2 == 0) {
+                    f[i][j] = 1;
+                } else {
+                    f[i][j] = 0;
+                }
+            }
+        }
+        for (int k = 0; k < length; k++) {
+            for (int m = 0; m < height; m++) {
+                System.out.print(f[k][m] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
